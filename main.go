0a3931a09308e44e3aef2e6c6ec854ca5c18b350
Benton Roberts <broberts@mdsol.com>
2017-04-20 07:55:53 -0700

    add broken branch

--- main.go
+++ main.go
@@ -12,13 +12,21 @@ func main() {
 	port := flag.Int("port", 80, "listening port")
 	flag.Parse()
 	fmt.Printf("Starting on port %d:...\n", *port)
-	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/", brokenHandler)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), nil))
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	host := strings.Split(r.Host, ":")[0]
-	msg := fmt.Sprintf("Request for %s%s -> OK", host, r.URL.Path)
-	log.Println(msg)
-	fmt.Fprintf(w, fmt.Sprintf("%s\n", msg))
+	msg := fmt.Sprintf("Request for %s%s -> OK\n", host, r.URL.Path)
+	log.Print(msg)
+	fmt.Fprintf(w, msg)
+}
+
+func brokenHandler(w http.ResponseWriter, r *http.Request) {
+	host := strings.Split(r.Host, ":")[0]
+	msg := fmt.Sprintf("Request for %s%s -> NOT OK! ☄\n", host, r.URL.Path)
+	log.Print(msg)
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(msg))
 }
